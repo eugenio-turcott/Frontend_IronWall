73496dd8657dee6d37dfa18929655811f6b2b3f6
CosmicBlue <A00833278@tec.mx>
2025-05-15 17:12:06 -0600

    Added mock data and alert list

--- src/Components/Alerts/AlertList.jsx
+++ src/Components/Alerts/AlertList.jsx
@@ -1,8 +1,19 @@
-import React from "react";
 import AlertComponent from "./AlertComponent";
 
 export default function AlertList({alerts}){
     return(
-        
+        <div>
+            <h1>Alerts</h1>
+            <div className="overflow-y-scroll h-full rounded p-2 flex flex-col space-y-4">
+                {alerts.map((item,index) => (
+                    <AlertComponent 
+                    key={index}
+                    tipo={item.tipo}
+                    nodo={item.nodo}
+                    fecha={item.fecha}
+                    />
+                ))}
+            </div>
+        </div>
     );
 }
--- src/Components/Alerts/Alerts.jsx
+++ src/Components/Alerts/Alerts.jsx
@@ -1,7 +1,19 @@
-import AlertComponent from "./AlertComponent";
-
+import AlertList from "./AlertList";
 export default function Alerts(){
+    const alerts = [
+    { tipo: "Falla de energía", nodo: "Nodo 1", fecha: "2025-05-15 09:00" },
+    { tipo: "Conexión perdida", nodo: "Nodo 2", fecha: "2025-05-15 10:15" },
+    { tipo: "Sobrecalentamiento", nodo: "Nodo 3", fecha: "2025-05-15 11:30" },
+    { tipo: "Sobrecalentamiento", nodo: "Nodo 3", fecha: "2025-05-15 11:30" },
+    { tipo: "Sobrecalentamiento", nodo: "Nodo 3", fecha: "2025-05-15 11:30" },
+    { tipo: "Sobrecalentamiento", nodo: "Nodo 3", fecha: "2025-05-15 11:30" },
+    { tipo: "Sobrecalentamiento", nodo: "Nodo 3", fecha: "2025-05-15 11:30" },
+    { tipo: "Sobrecalentamiento", nodo: "Nodo 3", fecha: "2025-05-15 11:30" },
+
+  ];
     return(
-        <AlertComponent />
+    <div>
+      <AlertList alerts={alerts} />
+    </div>
     );
 }
