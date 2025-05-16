20c19646d09031d02b6058f6172c9e0dca69261f
CosmicBlue <A00833278@tec.mx>
2025-05-15 21:52:20 -0600

    Agregando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./Components/Login";
+import Alerts from "./Components/Alerts/Alerts"
+import { Component } from "react";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/alerts" element={<Alerts />} />
     </Routes>
   );
 };
