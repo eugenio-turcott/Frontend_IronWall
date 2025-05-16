30f6899d943712f786a4efa6bc61fe2ce2ea95b5
eugenio-turcott <A01198189@tec.mx>
2025-05-15 18:12:52 -0600

    Cambios en Login (Front y Back)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Login from './Components/Login'
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./Components/Login";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <Login/>
-    </>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
